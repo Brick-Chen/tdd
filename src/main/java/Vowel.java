9786a6cf8b93aed2322860566cbb7ed04cf34e77
YumingChen <mathcym@outlook.com>
2020-09-14 12:34:25 +0800

    [yuming chen]: implement solution to string with vowels less than 30 percent.

--- src/main/java/Vowel.java
+++ src/main/java/Vowel.java
@@ -1,5 +1,6 @@
 public class Vowel {
     private static final String WORD = "mommy";
+    private static final double RATIO = 0.3;
 
     public String insertMommy(String s) {
         if (s == null) {
@@ -8,6 +9,35 @@ public class Vowel {
         if (s.length() == 0) {
             return "";
         }
-        return s;
+        if (!vowelRatioValid(s)) {
+            return s;
+        }
+        return "";
+    }
+
+    /**
+     * check whether the total number of vowels(a e i o u)
+     * count more than 30% of the length of string
+     * @param s String
+     * @return ture or false
+     */
+    private boolean vowelRatioValid(String s) {
+        double cnt = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c) && isVowel(Character.toLowerCase(c))) {
+                cnt += 1;
+            }
+        }
+        return cnt / s.length() > RATIO;
+    }
+
+    /**
+     * check whether a character is vowel(a e i o u)
+     * @param c character
+     * @return true or false
+     */
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c =='u';
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class MommifierTest {
     @Test
-    public void should_throw_null_exception_when_string_is_null() throws NullPointerException{
+    public void should_throw_null_exception_when_string_is_null(){
         Assertions.assertThrows(NullPointerException.class, ()->{
             Vowel vowel = new Vowel();
             vowel.insertMommy(null);
@@ -16,5 +16,13 @@ public class MommifierTest {
         String emptyString = "";
         String ans = vowel.insertMommy(emptyString);
         Assertions.assertEquals("", ans);
+    }
+
+    @Test
+    public void should_return_original_string_when_number_of_vowels_below_30_percent() {
+        Vowel vowel = new Vowel();
+        String vowelBelow30Percent = "pHiLIPs";
+        String ans = vowel.insertMommy(vowelBelow30Percent);
+        Assertions.assertEquals(vowelBelow30Percent, ans);
     }
 }
